c4ef080b9fe027cb032ce462d1d3edddc95f6826
fredrir <fhansteen@gmail.com>
2024-09-08 14:40:51 +0200

    changed text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Appkom  
         </a>
       </header>
     </div>
