a0598f8162ebb31c37184aebfbaadd76e272476e
Marie Holen Danilson <mariehd@ntnu.no>
2024-09-08 17:03:15 +0200

    laget en basic footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -2,12 +2,16 @@ import "./Footer.css";
 
 function Footer() {
   return (
-    <footer className="Test">
+    <footer className="Footer">
       <p>
         Har du ikke rukket å bli drita enda?
         <br />
         Kontakt: Fredrik
       </p>
+      <div className="logoer">
+      <a href="https://www.facebook.com/LinjeforeningenOnline"> <img src="/facebook ikon.svg" alt="Facebook logo"></img> </a>
+      <a href="https://www.instagram.com/online_ntnu/"> <img src="/instagram ikon.svg" alt="instagram logo"></img> </a>
+      </div>
     </footer>
   );
 }
