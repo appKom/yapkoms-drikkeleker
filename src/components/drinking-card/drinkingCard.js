ee85d65b67fba62b1169df09e9b7db01ab546616
fredrir <fhansteen@gmail.com>
2024-09-08 16:53:20 +0200

    add drinking game card

---
+++ src/components/drinking-card/drinkingCard.js
@@ -0,0 +1,16 @@
+import "./drinkingCard.css";
+
+function DrinkingCard() {
+  return (
+    <div className="drinking-card">
+      <img
+        className="Drinking Card"
+        alt="Drinking Card"
+        src="https://is3-ssl.mzstatic.com/image/thumb/Purple113/v4/70/82/34/70823436-6443-6f96-411b-8853e651852f/source/512x512bb.jpg"
+      ></img>
+      <h1 className="">Drinking Card</h1>
+    </div>
+  );
+}
+
+export default DrinkingCard;
