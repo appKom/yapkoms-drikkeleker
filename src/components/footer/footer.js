ff5cb8e9bf63d81d98241d7c7ee85eeb4d3eecd8
fredrir <fhansteen@gmail.com>
2024-09-08 16:16:20 +0200

    .

---
+++ src/components/footer/footer.js
@@ -0,0 +1,14 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer className="Test">
+      <p>
+        Har du ikke rukket å bli drita enda?
+        <br />
+        Kontakt: Fredrik
+      </p>
+    </footer>
+  );
+}
+export default Footer;
