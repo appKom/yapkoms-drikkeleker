f0bc1e262192dededc09929c62b8cd6a6ae98adb
Amund Berge <amundberge@dhcp-10-22-130-207.wlan.ntnu.no>
2024-09-08 16:42:24 +0200

    Added navbar component

---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,18 @@
+function Navbar(){
+    return(<nav>
+        <div id="container">
+            <div id="online-logo">
+              <a href="https://online.ntnu.no" id="logo-link"></a>
+            </div>
+            <div id="appkom-logo">
+                <div id="appkom-logo-container"></div>
+            </div>
+            <div id="sponsor">
+                <a href="https://www.bekk.no/" id="sponsor-link"></a>
+            </div>
+
+        </div>
+    </nav>)
+}
+
+export default Navbar
