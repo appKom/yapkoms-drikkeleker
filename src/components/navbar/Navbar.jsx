3bd9b1177ca2f720e00a67c9b5a09d77b86ad26e
fredrir <fhansteen@gmail.com>
2024-09-22 16:22:13 +0200

    feat: add navbar

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,23 @@
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div id="container">
+        <a href="https://www.online.ntnu.no" id="online-container">
+          <img src="/online-logo.svg" alt="Online logo" />
+        </a>
+
+        <a href="https://www.appkom.no" id="appkom-container">
+          <img src="/appkom-logo.svg" alt="Appkom logo" />
+        </a>
+
+        <a href="https://www.bekk.no" id="sponsor-container">
+          <img src="/bekk.svg" alt="Bekk logo" />
+        </a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
