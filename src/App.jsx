187c1d372f5716186e7ebf583742371056e8a66f
Amund Berge <amundpamund@icloud.com>
2024-09-27 12:55:45 +0200

    Started styling and implementing drink cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import DrinkingCard from "./components/drinkingCard/drinkingCard";
 
 function App() {
   return (
@@ -6,6 +7,9 @@ function App() {
       <header className="header">
         <h1>Yapkoms drikkeleker</h1>
       </header>
+      <div id="testing">
+        <DrinkingCard name={"6 minutes"} image={'./bekk.svg'} description={"Den beste leken."}/>
+      </div>
     </div>
   );
 }
